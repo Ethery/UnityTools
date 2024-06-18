99c88410b0495553bbb19392ebc5af3a2abb2ff8
Ethery <loic.meunier74@gmail.com>
2024-06-18 13:55:43 +0200

    Added a condition task.

---
+++ UnityTools/AI/BehaviourTree/Blocks/Condition.cs
@@ -0,0 +1,25 @@
+﻿namespace UnityTools.AI.BehaviourTree
+{
+	public class Condition : Task
+	{
+		public Task SuccessTask = null;
+		public Task FailTask = null;
+
+		public delegate bool ConditionCheck(Blackboard blackboard);
+		public ConditionCheck ConditionCheckFunction;
+
+		public sealed override ETaskStatus Tick(Blackboard blackboard)
+		{
+			if (ConditionCheck(blackboard))
+			{
+				return ETaskStatus.Success;
+			}
+			return ETaskStatus.Failed;
+		}
+
+		private bool ConditionCheck(Blackboard blackboard)
+		{
+			return ConditionCheckFunction(blackboard);
+		}
+	}
+}
